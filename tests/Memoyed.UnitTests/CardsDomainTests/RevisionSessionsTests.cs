b02660382b2bac7b90df84d4842bb3f14da5817c
Denis Karpovskiy <geext29@gmail.com>
2020-01-25 11:45:45 +0300

    Add a test for session constructor taking set

--- tests/Memoyed.UnitTests/CardsDomainTests/RevisionSessionsTests.cs
+++ tests/Memoyed.UnitTests/CardsDomainTests/RevisionSessionsTests.cs
@@ -12,7 +12,7 @@ namespace Memoyed.UnitTests.CardsDomainTests
     public class RevisionSessionsTests
     {
         [Fact]
-        public void RevisionSessionConstructor_DontPassCardBox_TakesSessionCardsFromEntireSet()
+        public void RevisionSessionConstructor_DontPassCardBoxId_TakesSessionCardsFromEntireSet()
         {
             // Arrange
             var cardBoxSet = new CardBoxSet(new CardBoxSetId(Guid.NewGuid()),
@@ -52,6 +52,50 @@ namespace Memoyed.UnitTests.CardsDomainTests
             Assert.Equal(2, session.SessionCards.Count());
             Assert.True(session.SessionCards.All(sc => sc.LearningCardId == card1.Id
                                                        || sc.LearningCardId == card2.Id));
+        }
+        
+        [Fact]
+        public void RevisionSessionConstructor_PassCardBoxId_TakesSessionCardsFromCardBox()
+        {
+            // Arrange
+            var cardBoxSet = new CardBoxSet(new CardBoxSetId(Guid.NewGuid()),
+                new CardBoxSetLanguage("Russian", _ => true),
+                new CardBoxSetLanguage("Norwegian", _ => true));
+
+            var cardBox1 = new CardBox(
+                new CardBoxId(Guid.NewGuid()),
+                cardBoxSet.Id, new CardBoxLevel(1), new CardBoxRevisionDelay(2));
+            
+            var cardBox2 = new CardBox(
+                new CardBoxId(Guid.NewGuid()),
+                cardBoxSet.Id, new CardBoxLevel(2), new CardBoxRevisionDelay(7));
+
+            cardBoxSet.AddCardBox(cardBox1);
+            cardBoxSet.AddCardBox(cardBox2);
+            
+            var card1 = new LearningCard(new LearningCardId(Guid.NewGuid()),
+                new LearningCardWord("Привет"), 
+                new LearningCardWord("Hei"),
+                null);
+
+            cardBoxSet.AddNewCard(card1, new UtcTime(new DateTime(2020, 1, 1)));
+            var card2 = new LearningCard(new LearningCardId(Guid.NewGuid()),
+                new LearningCardWord("Дом"),
+                new LearningCardWord("Hus"),
+                null);
+            cardBoxSet.AddNewCard(card2, new UtcTime(new DateTime(2020, 1, 1)));
+            
+            cardBoxSet.PromoteCard(card2.Id, new UtcTime(new DateTime(2020, 1, 1)));
+            
+            // Act
+            var session = new RevisionSession(new RevisionSessionId(Guid.NewGuid()),
+                cardBoxSet, cardBox2.Id, new UtcTime(new DateTime(2020, 1, 10)));
+            
+            // Assert
+            Assert.Collection(session.SessionCards, sc =>
+            {
+                Assert.Equal(sc.LearningCardId, card2.Id);
+            });
         }
     }
 }
