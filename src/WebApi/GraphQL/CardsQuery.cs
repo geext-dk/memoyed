f4afeb73fa73b79af91b0d01fb57b3da61b6a251
Denis Karpovskiy <geext29@gmail.com>
2020-04-26 01:32:57 +0300

    Add a query for querying revision sessions

--- src/WebApi/GraphQL/CardsQuery.cs
+++ src/WebApi/GraphQL/CardsQuery.cs
@@ -31,8 +31,31 @@ namespace Memoyed.WebApi.GraphQL
                                 FROM card_box_sets AS s";
 
                     var id = c.GetArgument<Guid?>("id");
-                    if (id.HasValue) sql += " WHERE @Id = s.Id";
+                    if (id.HasValue) sql += " WHERE @Id = s.id";
                     return await connection.QueryAsync<ReturnModels.CardBoxSetModel>(sql, new
+                    {
+                        Id = id
+                    });
+                });
+
+            FieldAsync<ListGraphType<RevisionSessionType>, IEnumerable<ReturnModels.RevisionSessionModel>>(
+                "revisionSessions",
+                arguments: new QueryArguments(new QueryArgument<IdGraphType>
+                {
+                    Name = "id",
+                    Description = "Id of a revision session to query"
+                }),
+                resolve: async c =>
+                {
+                    using var scope = serviceProvider.CreateScope();
+                    var connection = scope.ServiceProvider.GetRequiredService<IDbConnection>();
+                    var sql = @"SELECT rs.id, rs.status, rs.card_box_set_id 
+                                FROM revision_sessions AS rs";
+
+                    var id = c.GetArgument<Guid?>("id");
+                    if (id.HasValue) sql += " WHERE @Id = rs.id";
+
+                    return await connection.QueryAsync<ReturnModels.RevisionSessionModel>(sql, new
                     {
                         Id = id
                     });
