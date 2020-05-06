f8e27c9ebf365ca33c93f7dbb81ed3f5b17ba615
Denis Karpovskiy <geext29@gmail.com>
2020-05-06 20:11:26 +0300

    Add a buildah script to build the image

---
+++ buildah-script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# exit if one of the commands fail
+set -e
+
+build_con=$(buildah from --pull mcr.microsoft.com/dotnet/core/sdk:3.1)
+buildah run $build_con mkdir -p /app
+buildah config --workingdir /app $build_con
+
+echo "COPYING .csproj FILES"
+buildah copy $build_con src/DomainFramework/DomainFramework.csproj /app/DomainFramework/DomainFramework.csproj
+buildah copy $build_con src/Domain.Cards/Domain.Cards.csproj /app/Domain.Cards/Domain.Cards.csproj
+buildah copy $build_con src/Domain.Users/Domain.Users.csproj /app/Domain.Users/Domain.Users.csproj
+buildah copy $build_con src/Application/Application.csproj /app/Application/Application.csproj
+buildah copy $build_con src/WebApi/WebApi.csproj /app/WebApi/WebApi.csproj
+
+echo "RESTORING"
+buildah run $build_con dotnet restore /app/WebApi/WebApi.csproj
+
+echo "COPYING SOURCE CODE"
+buildah copy $build_con src/DomainFramework/ /app/DomainFramework
+buildah copy $build_con src/Domain.Cards/ /app/Domain.Cards
+buildah copy $build_con src/Domain.Users/ /app/Domain.Users
+buildah copy $build_con src/Application/ /app/Application
+buildah copy $build_con src/WebApi/ /app/WebApi
+buildah config --workingdir /app/WebApi $build_con
+
+echo "PUBLISHING"
+buildah run $build_con dotnet publish -c Release -o out
+
+build_mount=$(buildah mount $build_con)
+
+run_con=$(buildah from --pull mcr.microsoft.com/dotnet/core/aspnet:3.1)
+
+echo "CHANGING WORKING DIRECTORY"
+buildah config --workingdir /app $run_con
+
+echo "MOUNTING RUN CONTAINER"
+run_mount=$(buildah mount $run_con)
+
+echo "COPYING PUBLISHED APP"
+cp -r $build_mount/app/WebApi/out $run_mount/app
+
+buildah umount $build_con
+buildah umount $run_con
+
+buildah config --entrypoint '["dotnet", "Memoyed.WebApi.dll"]' $run_con
+
+echo "COMMITTING"
+buildah commit $run_con registry.gitlab.com/geext/memoyed
